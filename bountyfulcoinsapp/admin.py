f4d827c953a2d88cd27cf08483e865f55b259975
asfaltboy <asfaltboy@gmail.com>
2014-02-28 20:02:28 +0200

    style(admin): PEP8 the module (mostly whitespace)

--- bountyfulcoinsapp/admin.py
+++ bountyfulcoinsapp/admin.py
@@ -1,26 +1,29 @@
 from django.contrib import admin
 from bountyfulcoinsapp.models import *
 
+
 # Link Model
 class LinkAdmin(admin.ModelAdmin):
-	pass
+    pass
 admin.site.register(Link, LinkAdmin)
+
 
 # Bounty Model
 class BountyAdmin(admin.ModelAdmin):
-	list_display = ('title', 'link', 'user', 'amount', 'currency')
-	list_fitler = ('user',)
-	ordering = ('title',)
-	search_fields = ('title', )
+    list_display = ('title', 'link', 'user', 'amount', 'currency')
+    list_fitler = ('user',)
+    ordering = ('title',)
+    search_fields = ('title', )
 admin.site.register(Bounty, BountyAdmin)
+
 
 # Tag Model
 class TagAdmin(admin.ModelAdmin):
-	pass
+    pass
 admin.site.register(Tag, TagAdmin)
 
 
 # Shared Bounty
 class SharedBountyAdmin(admin.ModelAdmin):
-	pass
+    pass
 admin.site.register(SharedBounty, SharedBountyAdmin)
