779c44e18274d397ed82d49db65cd3e646366a47
Pavel Savchenko <pasha.savchenko@gmail.com>
2014-03-04 08:08:15 +0200

    chore: Add logging to decay_balance command

--- bountyfulcoinsapp/management/commands/decay_balance.py
+++ bountyfulcoinsapp/management/commands/decay_balance.py
@@ -1,6 +1,11 @@
+import logging
+
 from django.core.management.base import BaseCommand
 
 from bountyfulcoinsapp.models import Address
+
+
+logger = logging.getLogger('bountyfulcoinsapp.%s' % __name__)
 
 
 class Command(BaseCommand):
@@ -8,4 +13,5 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for addr in Address.objects.exclude(featured_bounty=None):
+            logger.info('Syncing balance for address %s', addr.address_id)
             addr.sync_verified_balance()
