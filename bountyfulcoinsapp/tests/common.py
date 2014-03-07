85866bedbb09ae19676fb51961627f692d124012
Pavel Savchenko <pasha.savchenko@gmail.com>
2014-03-07 18:38:51 +0200

    tests: remove unneeded calls in setUpClass

--- bountyfulcoinsapp/tests/common.py
+++ bountyfulcoinsapp/tests/common.py
@@ -1,13 +1,8 @@
-from django.contrib.auth import get_user_model
 
 
 class SiteDataMixin(object):
     fixtures = ['users', 'bounties']
     # existing users: admin | qwe123, test | test
-
-    @classmethod
-    def setUpClass(cls):
-        cls.User = get_user_model()
 
     def _fill_form(self, form, data):
         for field, value in data.iteritems():
